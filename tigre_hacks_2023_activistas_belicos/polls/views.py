6f5f45f32d1ba25f6fa3b1ddf2c2a0112e800606
Logan Martinez <69453107+Xil32@users.noreply.github.com>
2023-05-27 03:51:57 -0600

    Sign in view method and template folder pathed

--- tigre_hacks_2023_activistas_belicos/polls/views.py
+++ tigre_hacks_2023_activistas_belicos/polls/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello, world. You are at the polls index")
+
+def sign_in(request):
+    user_email = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=user_email, password=password)
+    
+    if user is not None:
+        login(request, user)
+        return HttpResponse("Signed in")
