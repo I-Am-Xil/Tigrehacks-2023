2dc4f043abf381f65070bafd9289bad1973632fa
Logan Martinez <logan_amp@outlook.com>
2023-05-27 20:58:18 -0600

    Update settings.py

--- tigre_hacks_2023_activistas_belicos/tigre_hacks_2023_activistas_belicos/settings.py
+++ tigre_hacks_2023_activistas_belicos/tigre_hacks_2023_activistas_belicos/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = os.environ.get("DJANGO_STATIC_URL", "/static/")
+STATIC_ROOT = os.environ.get("DJANGO_STATIC_ROOT", "./static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
